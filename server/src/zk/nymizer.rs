6d093ec9f97ccd8192cb9c7aab9e478f0fe70fb9
Dan Yamamoto <dan@iij.ad.jp>
2023-07-06 02:00:14 +0000

    change nymizer spec

--- server/src/zk/nymizer.rs
+++ server/src/zk/nymizer.rs
@@ -10,12 +10,13 @@ use std::collections::{HashMap, HashSet};
 
 pub struct PseudonymousSolutions {
     pub solutions: Vec<HashMap<Variable, Term>>,
-    pub deanon_map: HashMap<Term, Term>,
+    pub deanon_map: HashMap<NamedNode, Term>,
 }
 
 #[derive(Default)]
 pub struct Pseudonymizer {
-    mapping: HashMap<(Option<Variable>, Term), Term>,
+    iri_mapping: HashMap<NamedNode, NamedNode>,
+    literal_mapping: HashMap<Literal, NamedNode>,
 }
 
 impl Pseudonymizer {
@@ -24,33 +25,41 @@ impl Pseudonymizer {
         NamedNode::new_unchecked(format!("{}{}", PSEUDONYMOUS_IRI_PREFIX, val))
     }
 
-    fn generate_pseudonymous_var() -> Literal {
+    fn generate_pseudonymous_var() -> NamedNode {
         let val = nanoid!(21, &PSEUDONYM_ALPHABETS);
-        Literal::new_simple_literal(format!("{}{}", PSEUDONYMOUS_VAR_PREFIX, val))
+        NamedNode::new_unchecked(format!("{}{}", PSEUDONYMOUS_VAR_PREFIX, val))
     }
 
-    fn issue(&mut self, var: &Variable, term: &Term) -> Result<Term, ZkSparqlError> {
-        match term {
-            Term::NamedNode(n) if n.as_str().starts_with(SKOLEM_IRI_PREFIX) => Ok(term.clone()),
-            Term::NamedNode(n) if !n.as_str().starts_with(SKOLEM_IRI_PREFIX) => Ok(self
-                .mapping
-                .entry((Some(var.clone()), term.clone()))
-                .or_insert(Term::NamedNode(Self::generate_pseudonymous_iri()))
-                .clone()),
-            Term::Literal(_) => Ok(self
-                .mapping
-                .entry((Some(var.clone()), term.clone()))
-                .or_insert(Term::Literal(Self::generate_pseudonymous_var()))
-                .clone()),
-            _ => Err(ZkSparqlError::FailedBuildingPseudonymousSolution),
+    fn issue_iri(&mut self, iri: &NamedNode) -> NamedNode {
+        if iri.as_str().starts_with(SKOLEM_IRI_PREFIX) {
+            iri.clone()
+        } else {
+            self.iri_mapping
+                .entry(iri.clone())
+                .or_insert(Self::generate_pseudonymous_iri())
+                .clone()
         }
     }
 
-    fn get_inverse(&self) -> HashMap<Term, Term> {
-        self.mapping
+    fn issue_literal(&mut self, literal: &Literal) -> NamedNode {
+        let nym = Self::generate_pseudonymous_var();
+        self.literal_mapping.insert(literal.clone(), nym.clone());
+        nym
+    }
+
+    fn get_inverse(&self) -> HashMap<NamedNode, Term> {
+        let mut iri_mapping_inverse: HashMap<NamedNode, Term> = self
+            .iri_mapping
             .iter()
-            .map(|((_, t), nym)| (nym.clone(), t.clone()))
-            .collect()
+            .map(|(iri, nym)| (nym.clone(), Term::NamedNode(iri.clone())))
+            .collect();
+        let literal_mapping_inverse: HashMap<NamedNode, Term> = self
+            .literal_mapping
+            .iter()
+            .map(|(literal, nym)| (nym.clone(), Term::Literal(literal.clone())))
+            .collect();
+        iri_mapping_inverse.extend(literal_mapping_inverse);
+        iri_mapping_inverse
     }
 
     pub fn pseudonymize_solutions(
@@ -69,22 +78,22 @@ impl Pseudonymizer {
                             term.clone()
                         } else if var.as_str().starts_with(VC_VARIABLE_PREFIX) {
                             match term {
-                                Term::NamedNode(n) => {
-                                    if n.as_str().ends_with(SUBJECT_GRAPH_SUFFIX) {
-                                        Term::NamedNode(NamedNode::new(
-                                            &n.as_str()[0..(n.as_str().len()
-                                                - SUBJECT_GRAPH_SUFFIX.len())],
-                                        )?)
-                                    } else {
-                                        return Err(
-                                            ZkSparqlError::FailedBuildingPseudonymousSolution,
-                                        );
-                                    }
+                                Term::NamedNode(n)
+                                    if n.as_str().ends_with(SUBJECT_GRAPH_SUFFIX) =>
+                                {
+                                    Term::NamedNode(NamedNode::new(
+                                        &n.as_str()
+                                            [0..(n.as_str().len() - SUBJECT_GRAPH_SUFFIX.len())],
+                                    )?)
                                 }
                                 _ => return Err(ZkSparqlError::FailedBuildingPseudonymousSolution),
                             }
                         } else {
-                            self.issue(var, term)?
+                            match term {
+                                Term::NamedNode(n) => Term::NamedNode(self.issue_iri(n)),
+                                Term::Literal(l) => Term::NamedNode(self.issue_literal(l)),
+                                _ => return Err(ZkSparqlError::FailedBuildingPseudonymousSolution),
+                            }
                         };
                         Ok((var.clone(), pseudonymized_term))
                     })
